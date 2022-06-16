7906a9642062ab765d4530db83f8977647ad5ff2
Krzysztof Jakubowski <fakedperson@o2.pl>
2022-06-16 15:42:21 +0200

    9. Infrastructure
      - Nugets
        + AutoFac
        + Autofac.Extension.DependencyInjection
        + HangFire i HangFire.Core (nie wiem po co)
        + Microsoft.EntityFrameworkCore.Tools
        + Microsoft.EntityFrameworkCore.SqlServer
    
      - Rejestracja Mediatora
        + AutofacDependencyResolver.cs
        + MediatorConfiguration.cs

---
+++ SzkolenieTechniczne.Projekt.Cimena.Infrastructure/AutofacDependencyResolver.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using SzkolenieTechniczne.Projekt.Domain;
+
+namespace SzkolenieTechniczne.Projekt.Cimena.Infrastructure;
+
+public class AutofacDependencyResolver : IDependencyResolver
+{
+	private readonly ILifetimeScope _lifetimeScope;
+
+	public AutofacDependencyResolver(ILifetimeScope lifetimeScope)
+	{
+		_lifetimeScope = lifetimeScope;
+	}
+
+	public T? ResolveOrDefault<T>() where T : class => _lifetimeScope.ResolveOptional<T>();
+}
---
+++ SzkolenieTechniczne.Projekt.Cimena.Infrastructure/MediatorConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using SzkolenieTechniczne.Projekt.Domain;
+using SzkolenieTechniczne.Projekt.Domain.Command;
+using SzkolenieTechniczne.Projekt.Domain.Command.Add;
+using SzkolenieTechniczne.Projekt.Domain.Mediator;
+using SzkolenieTechniczne.Projekt.Domain.Query;
+
+namespace SzkolenieTechniczne.Projekt.Cimena.Infrastructure;
+
+public static class MediatorConfiguration
+{
+	public static void ConfigureMediator(this ContainerBuilder containerBuilder)
+	{
+		containerBuilder.RegisterType<Mediator>().As<IMediator>().InstancePerLifetimeScope();
+
+		containerBuilder
+			.Register(factory =>
+			{
+				var lifetimeScope = factory.Resolve<ILifetimeScope>();
+				return new AutofacDependencyResolver(lifetimeScope.BeginLifetimeScope());
+			})
+			.As<IDependencyResolver>()
+			.InstancePerLifetimeScope();
+
+		var handlersAssembly = typeof(AddMovieCommandHandler).Assembly;
+		containerBuilder
+			.RegisterAssemblyTypes(handlersAssembly)
+			.AsClosedTypesOf(typeof(ICommandHandler<>))
+			.InstancePerLifetimeScope();
+
+		containerBuilder
+			.RegisterAssemblyTypes(handlersAssembly)
+			.AsClosedTypesOf(typeof(IQueryHandler<,>))
+			.InstancePerLifetimeScope();
+	}
+}
